8d99f032a004ceef1063298e9dac6563f35559d8
adam.awizen <awizen.adam@gmail.com>
2023-05-06 17:15:41 +0200

    move showing expenses logc to ExpensesList.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -17,19 +17,6 @@ function Expenses(props) {
       return expense.date.getFullYear().toString() === filteredYear;
     });
 
-  let expensesContent = <p>No expenses found for year {filteredYear}</p>
-
-  if (filteredExpenses.length > 0) {
-      expensesContent = filteredExpenses.map(expense => {
-        return <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      });
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -37,7 +24,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={yearChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
